3dfb54a28776dda3b10d9e5b9e8e73510e1675bf
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-23 19:52:39 +0530

    Update vite.config.ts

--- project/frontend/vite.config.ts
+++ project/frontend/vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // assuming vite.config.ts and index.html are both in /frontend
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
-});
+})
