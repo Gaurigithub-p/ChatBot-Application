a3fb8e92d86e50a2724cdedf5642b0b4ed73e906
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-23 21:16:33 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: 'dist', // The output directory for the build
     emptyOutDir: true, // Ensure the output directory is cleared before building
     rollupOptions: {
-      input: 'project/frontend/index.html', // Specify the correct path to the entry file
+      input: 'project/index.html', // Specify the correct path to the entry file
     },
   },
 });
