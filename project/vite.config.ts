1b88d5ad43f4d8c9294a4e2e868355734124bac9
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-23 21:29:56 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'project',
   plugins: [react()],
   build: {
-    outDir: 'dist', // The output directory for the build
-    emptyOutDir: true, // Ensure the output directory is cleared before building
-    rollupOptions: {
-      input: 'project/index.html', // Specify the correct path to the entry file
-    },
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 });
