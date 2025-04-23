edb0b430d8daf8c181be2621f9831b3ead312444
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-23 21:37:48 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'project',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
