1398f2e3baa917694cf9589db67664d31f804923
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-30 17:37:25 +0530

    Update provider.tf

--- project/infra/provider.tf
+++ project/infra/provider.tf
@@ -1,4 +1,7 @@
-
-provider "aws" {
-  region = var.aws_region
+terraform {
+  backend "s3" {
+    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
