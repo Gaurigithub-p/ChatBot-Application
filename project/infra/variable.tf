59b3b147ccd840f9efd3b9358daa44a007b79e7d
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-30 13:07:45 +0530

    Update variable.tf

--- project/infra/variable.tf
+++ project/infra/variable.tf
@@ -1,11 +1,19 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-  default     = "chatbot-eks-cluster"
+  default = "chatbot-eks-cluster"
 }
