2094afda2f4ad0fa167f95092e711a0b273d2230
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-30 14:20:10 +0530

    Update main.tf

--- project/infra/main.tf
+++ project/infra/main.tf
@@ -4,12 +4,6 @@ data "aws_eks_cluster_auth" "cluster_auth" {
   name = aws_eks_cluster.eks_cluster.name
 }
 
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-}
-
 resource "aws_vpc" "chatbot_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
