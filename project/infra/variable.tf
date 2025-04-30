49fadb4ea8c35749cd9de922983e0090a16a1cec
Gaurigithub-p <gauripawar8692@gmail.com>
2025-04-30 14:14:40 +0530

    Update variable.tf

--- project/infra/variable.tf
+++ project/infra/variable.tf
@@ -1,16 +1,31 @@
+# variables.tf
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1" # Adjust the default region to your desired AWS region
+}
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnets" {
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "cluster_name" {
-  default = "chatbot-eks-cluster"
+  description = "EKS cluster name"
+  type        = string
+  default     = "chatbot-eks-cluster"
 }
