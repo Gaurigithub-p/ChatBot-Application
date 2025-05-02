b4725b6fa6b4dcdd80915f84d79803c4d45793d1
Gaurigithub-p <gauripawar8692@gmail.com>
2025-05-02 12:41:04 +0530

    Update main.tf

--- project/infra/main.tf
+++ project/infra/main.tf
@@ -1,113 +1,50 @@
-# IAM policy document
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-# EKS Cluster IAM Role
-resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
-}
-
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get AZs
-data "aws_availability_zones" "available" {}
-
-# Create public subnets in two AZs
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
-}
-
-# EKS Cluster
-resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.public_subnet_1.id,
-      aws_subnet.public_subnet_2.id
-    ]
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-  ]
-}
-
-# Node Group IAM Role
+# IAM Role for Node Group
 resource "aws_iam_role" "example1" {
   name = "eks-node-group-cloud"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
     Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
       }
     }]
+    Version = "2012-10-17"
   })
 }
 
+# IAM Role Policy Attachments (only after role is created)
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.example1.name
+  depends_on = [aws_iam_role.example1]
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.example1.name
+  depends_on = [aws_iam_role.example1]
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.example1.name
+  depends_on = [aws_iam_role.example1]
 }
 
-# Node Group
+# Create node group with explicit dependencies
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
+  subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
     desired_size = 1
     max_size     = 2
     min_size     = 1
   }
-
   instance_types = ["t2.medium"]
 
   depends_on = [
